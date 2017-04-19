8b40cedf464aad9de21d449d81aa189d4d7e75cb
Angel <asolis@e1z2r4p9.42.us.org>
2017-04-18 23:26:20 -0700

    ft_ls

--- main.c
+++ main.c
@@ -6,6 +6,16 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <stdlib.h>
+
+void	ft_swapchar(char **a, char **b)
+{
+	char *c;
+
+	c = ft_strdup(*a);
+	*a = ft_strdup(*b);
+	*b = ft_strdup(c);
+}
 
 int		print_value(char *file)
 {
@@ -16,7 +26,6 @@ int		print_value(char *file)
 
 	if(stat(file, &fileStat) < 0)
 			return 1;
-	// printf("-%s-\n", ctime(&fileStat.st_ctime));
 	pw = getpwuid(fileStat.st_uid);
 	gr = getgrgid(fileStat.st_gid);
 	ft_printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
@@ -34,18 +43,91 @@ int		print_value(char *file)
 	return (fileStat.st_blocks);
 }
 
-int main(int argc, char **argv)
+int			ft_lendir() //Len of directory
 {
-	static int i;
 	struct dirent *pDirent;
 	DIR *pDir;
+	int i;
 
 	pDir = opendir(".");
 	i = 0;
-	if(argc != 2)
-		return (1);
 	while ((pDirent = readdir(pDir)) != NULL)
-		print_value(pDirent->d_name);
+	if(pDirent->d_name[0] != '.')
+		i++;
+	closedir(pDir);
+	return (i);
+}
+
+void		ft_print_Matrix(char **matrix) //Print the matrix
+{
+	int j;
+
+	j = 0;
+	while(matrix[j] != NULL)
+	{
+		ft_printf("%10s", matrix[j]);
+		j++;
+	}
+}
+
+char		**ft_make_matrix(int size) // feed the matrix with the names of the files in a randoom way
+{
+	struct dirent *pDirent;
+	DIR *pDir;
+	int i;
+	char **matrix;
+
+	matrix = (char **)malloc(sizeof(char *) * (size + 1));
+	pDir = opendir(".");
+	i = 0;
+	while ((pDirent = readdir(pDir)) != NULL)
+		{
+			if(pDirent->d_name[0] != '.')
+				{
+					matrix[i] = ft_strdup(pDirent->d_name);
+					i++;
+				}
+		}
+		matrix[i] = NULL;
+	closedir(pDir);
+	return (matrix);
+}
+
+void		ft_ls() // ls unix comand
+{
+	int i;
+	int j;
+	int flag;
+	char **matrix;
+
+	flag = 1;
+	i = ft_lendir();
+	matrix = ft_make_matrix(i);
+	while (flag != 0)
+	{
+		flag = 0;
+		while(matrix[j] != NULL)
+		{
+			if (matrix[j + 1] && ft_strcmp(matrix[j], matrix[j + 1]) > 0)
+			{
+				ft_swapchar(&matrix[j], &matrix[j + 1]); //swaping the names
+				flag++;
+			}
+			j++;
+		}
+		j = 0;
+	}
+	ft_print_Matrix(matrix);
+}
+
+
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		ft_ls();
+
+
 	// print_value(argv[1]);
 	return (0);
 }
