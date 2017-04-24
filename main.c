cec2a46abd4f5d9fbb3a319212e0460d635dd2c8
Uzziel Solis Rios <Uzziel@MacBook-Air-de-Uzziel.local>
2017-04-24 03:07:01 -0700

    siiiiii

--- main.c
+++ main.c
@@ -53,20 +53,23 @@ int				print_permissions(struct	stat fileStat, char *file)
 	return (fileStat.st_blocks);
 }
 
-int				print_value(char *file)
+int				print_value(char *file, char *dir, char flag)
 {
 	char		date[36];
 	struct	stat fileStat;
 	struct	passwd *pw;
 	struct	group  *gr;
-
+	// printf("file = %s\n dir = %s\n", file, dir);
 	if(stat(file, &fileStat) < 0)
 			return 1;
 	pw = getpwuid(fileStat.st_uid);
 	gr = getgrgid(fileStat.st_gid);
 	print_permissions(fileStat, file);
 	ft_printf("  %d %s  %s %7d ", fileStat.st_nlink, pw->pw_name, gr->gr_name, fileStat.st_size);
-	ft_printf("%s %s\n", ft_strsub(ctime(&fileStat.st_ctime), 4, 12), file);
+	if (flag == 1)
+		ft_printf("%s %s\n", ft_strsub(ctime(&fileStat.st_ctime), 4, 12), file);
+	else if(flag == 0)
+		ft_printf("%s %s\n", ft_strsub(ctime(&fileStat.st_ctime), 4, 12), dir);
 	return (fileStat.st_blocks);
 }
 
@@ -86,27 +89,26 @@ int				ft_lendir(char *file) //Len of directory
 	return (i);
 }
 
-void			ft_print_Matrix(char **matrix, int flag) //Print the matrix
+void			ft_print_Matrix(char **matrix, int flag, char **direction) //Print the matrix
 {
 	int			j;
 
-	j = 0;
+	j = -1;
 	if (flag == 1)
 	{
-		while(matrix[j] != NULL)
-		{
+		while(matrix[++j] != NULL)
 			ft_printf("%s\n", matrix[j]);
-			j++;
-		}
 	}
-	else
+	else if (flag == 0)
 	{
-		while(matrix[j] != NULL)
-		{
-			print_value(matrix[j]);
-			j += 1;
-		}
+		while(matrix[++j] != NULL)
+			print_value(matrix[j], direction[j], 1);
 	}
+	else if (flag == 2)
+		{
+			while(matrix[++j] != NULL)
+				print_value(matrix[j], direction[j], 0);
+		}
 }
 
 char			**ft_make_matrix(int size, char *file) // feed the matrix with the names of the files in a randoom way
@@ -136,28 +138,19 @@ char			**ft_make_matrix(int size, char *file) // feed the matrix with the names 
 char		**ft_ls(t_ls *f, char *file) // ls unix comand
 {
 	char **matrix;
-	char *a;
-	char *b;
-	int		i;
-
-	i = 0;
 	start(f);
-	i = ft_lendir(file);
-	matrix = ft_make_matrix(i, file);
+	f->t.i = ft_lendir(file);
+	matrix = ft_make_matrix(f->t.i, file);
 	while (f->t.flag != 0)
 	{
 		f->t.flag = 0;
 		while(matrix[f->t.j] != NULL)
 		{
-			a =  matrix[f->t.j];
-			b =  matrix[f->t.j + 1];
 			if (matrix[f->t.j + 1] && ft_strcmp(matrix[f->t.j], matrix[f->t.j + 1]) > 0)
 			{
 				ft_swapchar(&matrix[f->t.j], &matrix[f->t.j + 1]); //swaping the names
 				f->t.flag += 1;
 			}
-			a =  matrix[f->t.j];
-			b =  matrix[f->t.j + 1];
 			f->t.j++;
 		}
 		f->t.j = 0;
@@ -185,7 +178,7 @@ void			ft_ls_R(t_ls *f, char *file)
 	if ((ft_strcmp(file, ".") != 0))
 		ft_printf("%s : \n", file);
 	matrix = ft_ls(f, file);
-	ft_print_Matrix(matrix, 1);
+	ft_print_Matrix(matrix, 1, f->t.matrix);
 	while (matrix[i] != NULL)
 	{
 		tmp = ft_strjoin(file, "/");
@@ -210,7 +203,27 @@ void			ft_ls_l(t_ls *f, char *file)
 	f->t.i = ft_lendir(file);
 	if (f->t.i != 1)
 		ft_printf("total %d\n", getting_all_blocks(matrix, f));
-	ft_print_Matrix(matrix, 0);
+	ft_print_Matrix(matrix, 0, f->t.matrix);
+}
+
+char				**getting_direction(char **matrix, char *file,  t_ls *f)
+{
+	int i;
+	char			*tmp;
+	char			*tmp2;
+	char			**tmp3;
+
+	tmp3 = (char**)malloc(sizeof(char*) * (f->t.i + 1));
+	tmp3[f->t.i] = NULL;
+	i = 0;
+	while (matrix[i] != NULL)
+	{
+		tmp = ft_strjoin(file, "/");
+		tmp2 = ft_strjoin(tmp, matrix[i]);
+		tmp3[i] = ft_strdup(tmp2);
+		i++;
+	}
+	return (tmp3);
 }
 
 void			ft_ls_R_2(t_ls *f, char *file)
@@ -218,24 +231,22 @@ void			ft_ls_R_2(t_ls *f, char *file)
 	char **matrix;
 	int i;
 	struct	stat fileStat;
-	char *tmp;
+	char **direction;
+
 	i = 0;
 	if ((ft_strcmp(file, ".") != 0))
 		ft_printf("%s : \n", file);
 	matrix = ft_ls(f, file);
-
-	while (matrix[i] != NULL)
+	direction = getting_direction(matrix, file, f);
+	ft_printf("total %d\n", getting_all_blocks(direction, f));
+	ft_print_Matrix(direction, 2, matrix);
+	while (direction[i] != NULL)
 	{
-		tmp = ft_strjoin(file, "/");
-		tmp = ft_strjoin(tmp, matrix[i]);
-		matrix[i] = ft_strdup(tmp);
-		print_value(matrix[i]);
-		stat(tmp, &fileStat);
+		stat(direction[i], &fileStat);
 		if(S_ISDIR(fileStat.st_mode))
 			{
-
-				ft_printf("\n", file);
-				ft_ls_R_2(f, matrix[i]);
+				ft_printf("\n");
+				ft_ls_R_2(f, direction[i]);
 			}
 		i++;
 	}
@@ -244,13 +255,12 @@ void			ft_ls_R_2(t_ls *f, char *file)
 int			main(int argc, char **argv)
 {
 	t_ls	*f;
-
-
+	char **direction;
 	if(!(f = (t_ls*)malloc(sizeof(t_ls))))
 		return (0);
 	start(f);
 	if (argc == 1)
-		ft_print_Matrix(ft_ls(f, "."), 1);
+		ft_print_Matrix(ft_ls(f, "."), 1, f->t.matrix);
 	if (argc == 2 && (ft_strcmp(argv[1], "-l") == 0))
 		ft_ls_l(f, ".");
 	if (argc == 2 && (ft_strcmp(argv[1], "-R") == 0))
